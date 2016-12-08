66cd66b09d611963920cdcd53e0751ed797a1608
Alckevich <alckevich@live.com>
2016-12-08 23:33:02 +0300

    Fixed tests.

--- app/index.test.js
+++ app/index.test.js
@@ -1,5 +1,5 @@
 import ngModule from './index.js';
 
 require('angular-mocks/angular-mocks');
-require('./modules/identity/client-list/client-list.test.js').default(ngModule);
-require('./modules/identity/create-client/create-client.test.js').default(ngModule);
+require('./modules/atm/login/login.test.js').default(ngModule);
+require('./modules/atm/home/home.test.js').default(ngModule);
